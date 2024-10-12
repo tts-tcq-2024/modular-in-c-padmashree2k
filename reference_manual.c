e29e5843212992a40dd88662719760aa648b7734
padmashree2k <95606111+padmashree2k@users.noreply.github.com>
2024-10-12 14:55:10 +0530

    Create reference_manual.c

---
+++ reference_manual.c
@@ -0,0 +1,10 @@
+#include "color_pair.h"
+#include <stdio.h>
+
+int main() {
+    // Print reference manual for wiring personnel
+    printf("\nPrinting color code reference manual:\n");
+    printColorReferenceManual();
+
+    return 0;
+}
