02d8b00fd3ec26e216d98d253f0df72f9ff8571b
padmashree2k <95606111+padmashree2k@users.noreply.github.com>
2024-10-12 14:53:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,3 +6,35 @@ int main() {
     print_color_reference();
     return 0;
 }
+#include "color_pair.h"
+#include <stdio.h>
+#include <assert.h>
+
+// Test functions for validation
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[16];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber) {
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
+
+int main() {
+    // Testing existing functionality
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
